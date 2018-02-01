8655fcd11d52dff02de379226a14615438b3bb02
Jatin Katyal <jatin.katyal13@gmail.com>
2018-02-01 23:29:34 +0530

    File read added

--- src/Example/Example1.java
+++ src/Example/Example1.java
@@ -5,29 +5,40 @@ import Encrypter.Encrypt;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CharsetEncoder;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class Example1 {
 
-    String readFile(String path, Charset encoding) throws IOException {
+    static String readFile(String path, Charset encoding) throws IOException {
         byte[] encoded = Files.readAllBytes(Paths.get(path));
         return new String(encoded, encoding);
     }
 
-    void writeFile(String path, String content) throws IOException {
+    static void writeFile(String path, String content) throws IOException {
         Files.write(Paths.get(path), content.getBytes());
     }
 
     public static void main(String args[]) {
 
-        Encrypt encrypt = new Encrypt("something", false);
-        String val = encrypt.encrypt("Jatin");
-        System.out.println(val);
+        String filePath = "/Users/jatinkatyal/IdeaProjects/CloudEncrypter/temp.txt";
+        String dFilePath = "/Users/jatinkatyal/IdeaProjects/CloudEncrypter/temp.txt";
+        String eFilePath = "/Users/jatinkatyal/IdeaProjects/CloudEncrypter/encrypted.txt";
 
-        Encrypt decrypt = new Encrypt("something", false);
-        String val2 = decrypt.decrypt(val);
-        System.out.println(val2);
+        try {
+            Encrypt encrypt = new Encrypt("something", false);
+            String val = encrypt.encrypt(readFile(filePath, Charset.forName("UTF-8")));
+            writeFile(eFilePath, val);
+
+            Encrypt decrypt = new Encrypt("something", false);
+            String read = readFile(eFilePath, Charset.forName("UTF-8"));
+            String val2 = decrypt.decrypt(read);
+            writeFile(dFilePath, val2);
+        } catch(IOException e) {
+            System.out.println(e.toString());
+        }
     }
 
 }
